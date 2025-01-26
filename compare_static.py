b070718491a1f0b48ffa1c6e970e6f12f40b4c95
Alexandre Moleiro <avataranedotas@yahoo.com>
2025-01-26 18:29:42 +0000

    Create compare_static.py

---
+++ compare_static.py
@@ -0,0 +1,34 @@
+import json
+from deepdiff import DeepDiff
+from datetime import datetime
+
+
+# Load dictionaries from JSON files
+def load_dict_from_json(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
+# File paths
+file1 = "LATEST_static.json"
+file2 = "PREVIOUS_static.json"
+
+# Load dictionaries
+latest = load_dict_from_json(file1)
+previous = load_dict_from_json(file2)
+
+
+diff = DeepDiff(
+    latest,
+    previous,
+    exclude_regex_paths=[r".*['\"]lastUpdated['\"].*"]
+)
+
+print (diff)
+
+timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+with open(f"changes.txt", "a") as file:
+    file.write (str(timestamp))
+    file.write ("\n")
+    json.dump(diff, file, indent=4)
+  
