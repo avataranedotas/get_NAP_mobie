d47d0556c7fbab51e6d19da461f35e8c91bd561e
Alexandre Moleiro <avataranedotas@yahoo.com>
2025-01-26 16:21:45 +0000

    Update get_dynamic.sh

--- get_dynamic.sh
+++ get_dynamic.sh
@@ -9,7 +9,7 @@ rm PREVIOUS_dynamic.xml
 mv LATEST_dynamic.xml PREVIOUS_dynamic.xml
 
 rm PREVIOUS_dynamic.json
-mv LATEST_dynamic.xml PREVIOUS_dynamic.json
+mv LATEST_dynamic.json PREVIOUS_dynamic.json
 
 #ir buscar os ficheiros
 wget -O LATEST_dynamic.xml "https://pgm.mobie.pt/integration/nap/evActualStatus" --no-check-certificate
