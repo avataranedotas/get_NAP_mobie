035171d819e71f71247c786cc332c6d0f9841c02
Alexandre Moleiro <avataranedotas@yahoo.com>
2025-08-24 09:23:52 +0100

    Update compare_static.py

--- compare_static.py
+++ compare_static.py
@@ -111,9 +111,11 @@ with open("changes.txt", "a", encoding="utf-8") as file:
         json.dump(modificados, file, indent=4, ensure_ascii=False)
         file.write("\n")
 
-with open("changes_add.json", "a", encoding="utf-8") as file:
+with open("changes_add.json", "w", encoding="utf-8") as file:
     if adicionados:
         json.dump(adicionados, file, indent=4, ensure_ascii=False)
+#chama a conversao adicionados para OSM
+import convert4
         
 
 
