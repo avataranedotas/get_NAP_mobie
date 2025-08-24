af8970f8b2048f4183ff22a385a31b5bb4b39046
Alexandre Moleiro <avataranedotas@yahoo.com>
2025-08-24 08:49:51 +0100

    Update compare_static.py

--- compare_static.py
+++ compare_static.py
@@ -111,7 +111,7 @@ with open("changes.txt", "a", encoding="utf-8") as file:
         json.dump(modificados, file, indent=4, ensure_ascii=False)
         file.write("\n")
 
-with open("changes_add.txt", "a", encoding="utf-8") as file:
+with open("changes_add.json", "a", encoding="utf-8") as file:
     if adicionados:
         json.dump(adicionados, file, indent=4, ensure_ascii=False)
         
